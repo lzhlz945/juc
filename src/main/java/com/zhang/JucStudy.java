4bdf1485583034c107e1ead2dbdf3b5e89c8583d
Administrator <1746678613@qq.com>
2020-10-26 15:06:34 +0800

    juc- 项目初始

---
+++ src/main/java/com/zhang/JucStudy.java
@@ -0,0 +1,19 @@
+package com.zhang;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author: create by zhl
+ * @version: v1.0
+ * @description: com.zhang
+ * @date:2020/10/26
+ */
+public class JucStudy {
+    @Test
+    public void test01(){
+        System.out.println("juc-初始化");
+
+
+    }
+
+}
