4d26ce51fd6ce359743b72e63fa448b02c1d80d7
Administrator <1746678613@qq.com>
2020-12-10 10:31:51 +0800

    juc 教程练习3

--- src/main/java/com/zhang/study/TestVolatile.java
+++ src/main/java/com/zhang/study/TestVolatile.java
@@ -11,7 +11,7 @@ package com.zhang.study;
 public class TestVolatile {
 	
 	public static void main(String[] args) {
-		ThreadDemo td = new ThreadDemo();
+		ThreadDemo1 td = new ThreadDemo1();
 		new Thread(td).start();
 		
 		while(true){
@@ -25,7 +25,7 @@ public class TestVolatile {
 
 }
 
-class ThreadDemo implements Runnable {
+class ThreadDemo1 implements Runnable {
 
 	private volatile boolean flag = false;
 
